42f28d42d392474bcbb423fc731adfcd6cf4a617
travisbartholome <travis.bartholome@gmail.com>
2017-02-07 22:34:52 -0600

    Change speed for second button. My planned approach doesn't work, Web Audio API doesn't work in the way I thought. This is going to be harder than anticipated. Who could have imagined that?

--- sound.js
+++ sound.js
@@ -5,29 +5,36 @@ function onDOMLoad() {
   let audioContext = new AudioContext();
   let sampleBuffer;
 
-  function playBuffer(buffer) {
-    sampleBuffer = buffer;
-    let player = audioContext.createBufferSource();
-    player.buffer = buffer;
-    player.connect(audioContext.destination);
-    let startTime = audioContext.currentTime + 0.5; // Is the 0.5s delay necessary?
-    player.start(startTime);
-    player.stop(startTime + 8); // Arbitrary duration
+  function playBuffer(playbackRate) {
+    return function(buffer) {
+      sampleBuffer = buffer;
+      let player = audioContext.createBufferSource();
+      player.buffer = buffer;
+      player.playbackRate.value = playbackRate;
+      //player.detune.value = 2400 * (1 - playbackRate); // HACK HACK HACK HACK for only 1/2 speed
+      player.connect(audioContext.destination);
+      let startTime = audioContext.currentTime + 0.5; // Is the 0.5s delay necessary?
+      player.start(startTime);
+      player.stop(startTime + 8); // Arbitrary duration
+    };
   }
 
-  function getAudio() {
-    if (sampleBuffer) {
-      playBuffer(sampleBuffer);
-    } else {
-      let request = new XMLHttpRequest();
-      request.onload = function() {
-        audioContext.decodeAudioData(request.response, playBuffer);
-      };
-      request.responseType = "arraybuffer";
-      request.open("GET", audioURL);
-      request.send();
-    }
+  function getAudio(playbackRate) {
+    return function() {
+      if (sampleBuffer) {
+        playBuffer(playbackRate)(sampleBuffer);
+      } else {
+        let request = new XMLHttpRequest();
+        request.onload = function() {
+          audioContext.decodeAudioData(request.response, playBuffer(playbackRate));
+        };
+        request.responseType = "arraybuffer";
+        request.open("GET", audioURL);
+        request.send();
+      }
+    };
   }
-  play.addEventListener("click", getAudio);
-  halfSpeed.addEventListener("click", getAudio);
+
+  play.addEventListener("click", getAudio(1));
+  halfSpeed.addEventListener("click", getAudio(0.5));
 }
